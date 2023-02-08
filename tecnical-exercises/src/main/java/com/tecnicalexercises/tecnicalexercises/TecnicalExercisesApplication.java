ec6e6d7bd8bfa6069e25a06c2b71e6bea53cefcd
davibrito1218 <davibrito1218@gmail.com>
2023-02-08 08:46:22 +0000

    initial commit

---
+++ tecnical-exercises/src/main/java/com/tecnicalexercises/tecnicalexercises/TecnicalExercisesApplication.java
@@ -0,0 +1,13 @@
+package com.tecnicalexercises.tecnicalexercises;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecnicalExercisesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TecnicalExercisesApplication.class, args);
+	}
+
+}
---
+++ tecnical-exercises/src/test/java/com/tecnicalexercises/tecnicalexercises/TecnicalExercisesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tecnicalexercises.tecnicalexercises;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TecnicalExercisesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
